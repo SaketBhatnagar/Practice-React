a0e31b28dfce5cd8c5d3ac4203c5791e32ea4027
Saket Bhatnagar <bhatnagarsaket7367@gmail.com>
2023-01-19 16:08:22 +0530

    search countries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Form from "./components/Form";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div></div>
+      <Form></Form>
+    </div>
+  );
 };
 
 export default App;
