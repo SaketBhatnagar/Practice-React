da6da0625a5618fafb76bbbd6d4b0bbf911f14bd
Saket Bhatnagar <bhatnagarsaket7367@gmail.com>
2023-02-13 14:39:20 +0530

    added search feat - api.gtihub.com.users project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,52 +1,63 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Main = () => {
-  //   let [dat, setData] = useState([]);
-  //   useEffect(() => {
-  //     // side-effects
-  //     let fetchData = async () => {
-  //       try {
-  //         let res = await fetch("https://api.github.com/users");
-  //         let data1 = await res.json();
-  //         setData(data1);
-  //       } catch (err) {
-  //         console.log(err);
-  //       }
-  //     };
-  //     fetchData();
-  //   }, []);
+  let [user, setUser] = useState("");
+  let [data, setData] = useState([]);
+  let [search, setSearch] = useState(false);
 
-  let [username, setUsername] = useState("");
-  let [password, setPassword] = useState("");
+  let [ulist, setUlist] = useState([]);
+  // `https://api.gtihub.com/users/${user}`
+
+  useEffect(() => {
+    let fetchData = () => {
+      fetch(`https://api.github.com/users/${user}`).then(res => {
+        res.json().then(data => {
+          setData([data]);
+          console.log(data);
+        });
+      });
+    };
+
+    if (search) {
+      fetchData();
+    }
+    setSearch(false);
+    console.log("useEffect");
+    console.log(data);
+  }, [search]);
 
   let handleSubmit = e => {
     e.preventDefault();
-    console.log(username);
-    console.log(password);
-    console.log("form submitted");
+    console.log("submitted");
+    setSearch(true);
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        Username{" "}
-        <input
-          type="text"
-          name="username"
-          onChange={e => setUsername(e.target.value)}
-        />
-        <br />
-        <br />
-        Password{" "}
-        <input
-          type="password"
-          name="password"
-          onChange={e => setPassword(e.target.value)}
-        />
-        <br />
-        <br />
-        <button>Submit</button>
-      </form>
-    </div>
+    <section>
+      <article>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search User...."
+            onChange={e => {
+              setUser(e.target.value);
+            }}
+          ></input>
+          {}
+
+          <button>🔍</button>
+        </form>
+
+        <section>
+          {data.map(value => {
+            return;
+            <>
+              <h1>{value.html_url}</h1>
+              <img src={value.avatar_url} width="300px" />
+            </>;
+          })}
+        </section>
+      </article>
+    </section>
   );
 };
 
