018b03c51e7c0676413e2594ac1e0dee91a2e317
Saket Bhatnagar <bhatnagarsaket7367@gmail.com>
2023-02-20 18:39:10 +0530

    useRef hook

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { userStore } from "../apis/Store";
 
+// useRef hook
 const Card = () => {
   let { data } = useContext(userStore);
+  let divRef = useRef("");
+
   console.log(data);
+
+  let handleColor = () => {
+    console.log(divRef);
+    divRef.current.style.backgroundColor = "red";
+    console.log(divRef);
+  };
 
   if (data) {
     return (
-      <div>
+      <div ref={divRef}>
         <h1>{data[0].base}</h1>
         <h2>{data[0].name}</h2>
         <h3>
           {data[0].main.temp} <sup>o</sup>C
         </h3>
+        <button onClick={handleColor}> Change Color </button>
       </div>
     );
   } else {
